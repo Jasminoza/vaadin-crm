bd8d13d5201667d2e7032f1abc095073434432b6
Nikita Yolkin <nikita.v.elkin@gazprombank.ru>
2024-01-10 18:15:33 +0300

    Handling View State

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -13,26 +13,27 @@ import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-import java.util.Collections;
-
 @PageTitle("Contacts | Vaadin CRM")
 @Route(value = "")
 public class ListView extends VerticalLayout {
   private final Grid<Contact> contactsGrid = new Grid<>(Contact.class);
   private final TextField filterText = new TextField();
-  private ContactForm form;
-  private CrmService service;
 
-  public ListView(CrmService service) {
-    this.service = service;
+  private final CrmService crmService;
+
+  private ContactForm contactForm;
+
+  public ListView(CrmService crmService) {
+    this.crmService = crmService;
 
     addClassName("list-view");
     setSizeFull();
     configureGrid();
-    configureForm();
+    configureContactForm();
 
     add(getToolbar(), getContent());
     updateList();
+    closeEditor();
   }
 
   private void configureGrid() {
@@ -42,17 +43,53 @@ public class ListView extends VerticalLayout {
     contactsGrid.addColumn(contact -> contact.getStatus().getName()).setHeader("Status");
     contactsGrid.addColumn(contact -> contact.getCompany().getName()).setHeader("Company");
     contactsGrid.getColumns().forEach(col -> col.setAutoWidth(true));
+
+    contactsGrid
+      .asSingleSelect()
+      .addValueChangeListener(event -> editContact(event.getValue()));
   }
 
-  private void configureForm() {
-    form = new ContactForm(service.findAllCompanies(), service.findAllStatuses());
-    form.setWidth("25em");
+  private void editContact(Contact contact) {
+    if (contact == null) {
+      closeEditor();
+    }
+
+    contactForm.setContact(contact);
+    contactForm.setVisible(true);
+    addClassName("editing");
+  }
+
+  private void closeEditor() {
+    contactForm.setContact(null);
+    contactForm.setVisible(false);
+    removeClassName("editing");
+  }
+
+  private void configureContactForm() {
+    contactForm = new ContactForm(crmService.findAllCompanies(), crmService.findAllStatuses());
+    contactForm.setWidth("25em");
+
+    contactForm.addSaveListener(this::saveContact);
+    contactForm.addDeleteListener(this::deleteContact);
+    contactForm.addCloseListener(e -> closeEditor());
+  }
+
+  private void saveContact(ContactForm.SaveEvent saveEvent) {
+    crmService.saveContact(saveEvent.getContact());
+    updateList();
+    closeEditor();
+  }
+
+  private void deleteContact(ContactForm.DeleteEvent deleteEvent) {
+    crmService.deleteContact(deleteEvent.getContact());
+    updateList();
+    closeEditor();
   }
 
   private Component getContent() {
-    HorizontalLayout content = new HorizontalLayout(contactsGrid, form);
+    HorizontalLayout content = new HorizontalLayout(contactsGrid, contactForm);
     content.setFlexGrow(2, contactsGrid);
-    content.setFlexGrow(1, form);
+    content.setFlexGrow(1, contactForm);
     content.addClassName("content");
     content.setSizeFull();
     return content;
@@ -65,13 +102,19 @@ public class ListView extends VerticalLayout {
     filterText.addValueChangeListener(e -> updateList());
 
     Button addContactButton = new Button("Add contact");
+    addContactButton.addClickListener(click -> addContact());
 
     var toolbar = new HorizontalLayout(filterText, addContactButton);
     toolbar.setClassName("toolbar");
     return toolbar;
   }
 
+  private void addContact() {
+    contactsGrid.asSingleSelect().clear();
+    editContact(new Contact());
+  }
+
   private void updateList() {
-    contactsGrid.setItems(service.findAllContacts(filterText.getValue()));
+    contactsGrid.setItems(crmService.findAllContacts(filterText.getValue()));
   }
 }
